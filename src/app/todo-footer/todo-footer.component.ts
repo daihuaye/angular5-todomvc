72faa8b9e725ea22a75062c9608dca9d2becd830
daihua ye <davishacker86@gmail.com>
2017-11-12 20:38:35 -0800

    add todo footer

---
+++ src/app/todo-footer/todo-footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-footer',
+  templateUrl: './todo-footer.component.html',
+  styleUrls: ['./todo-footer.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class TodoFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
